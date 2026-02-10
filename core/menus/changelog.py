e8b46d6c4f7c6cb7f14993c4a42acabed682a589
rugerclaus <rogerrock12games@gmail.com>
2026-02-10 03:15:22 -0700

    updated changelog.py for more robustness

--- core/menus/changelog.py
+++ core/menus/changelog.py
@@ -1,20 +1,44 @@
 from core.ui.lefttext import LeftAlignedText
+from helper import log_error
 
 class ChangeLog(LeftAlignedText):
     def __init__(self, board_surface):
         super().__init__(board_surface)
         self.changelog_text = []
+        self.max_char_count = 65
         self.load_changelog_from_file("changelog.txt")
 
     def load_changelog_from_file(self, file_path):
         try:
             with open(file_path, "r") as f:
-                self.changelog_text = [line.strip() for line in f.readlines()]
+                raw_lines = [line.strip() for line in f.readlines()]
+                for line in raw_lines:
+                    self.changelog_text.extend(self._break_line_into_chunks(line))
         except FileNotFoundError:
-            print("Changelog file not found, using default empty changelog.")
+            self.changelog_text = ["Changelog not found. Please email dev@snowblitz.net with your error.log, \n and event.log from /logs as well as a description of your issue"]
+            log_error("Changelog file not found, please send your error and event log files in /logs")
+
+    def _break_line_into_chunks(self, line):
+        words = line.split(' ')
+        current_line = ''
+        lines = []
+
+        for word in words:
+            if len(current_line) + len(word) + 1 > self.max_char_count:
+                lines.append(current_line)
+                current_line = word
+            else:
+                if current_line:
+                    current_line += ' ' + word
+                else:
+                    current_line = word
+
+        if current_line:
+            lines.append(current_line)
+
+        return lines
 
     def draw(self):
-
         if not self.changelog_text:
             self._draw_left_aligned_text("No changelog available.")
             return
